f1e3f44fa099b7dbd37c8b3e3bed6108b5407e31
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2026-03-09 21:33:43 +0700

    chore : add a landing page instead of kinraidee

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router"
+import LandingPage from "@/pages/LandingPage.vue"
 import Kinraidee from "../pages/Kinraidee.vue"
 
 const routes = [
   {
     path: "/",
     name: "home",
+    component: LandingPage
+  },
+   {
+    path: "/kinraidee",
+    name: "kinraidee",
     component: Kinraidee
   }
 ]
